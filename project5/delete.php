5ab475eb4f08295da47de19456e85e3a819e49fc
Jefri Kurniawan <jkurniawan057@gmail.com>
2023-07-25 21:14:53 +0700

    update

--- project5/delete.php
+++ project5/delete.php
@@ -4,12 +4,6 @@ if (isset($_GET['no'])) {
   $no = $_GET['no'];
   $query = "DELETE FROM siswa WHERE no='$no'";
   mysqli_query($conn, $query);
-  if ($hasil) {
-    $_SESSION['status'] = 'berhasil dihapus';
-    header("location:list_siswa.php?status=sukses");
-  } else {
-    $_SESSION['status'] = 'gagal dihapus';
-    header("location:list_siswa.php?status=gagal");
-  }
+  header("location:list_siswa.php?status=sukses");
 }
 ?>
